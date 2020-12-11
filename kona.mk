7ed435c52e812df91f0818950f8f30c13358e07f
Abdullah <abdullahtabbal@github.com>
2020-12-11 20:28:33 +0000

    sm8250-common: Support Alipay and WeChat fingerprint payment
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- kona.mk
+++ kona.mk
@@ -242,6 +242,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
+# IFAA manager
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
